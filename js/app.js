8164892e1f0e3bb7820e7c5c650134e558f7f258
CescDevGit07 <cesar04071997@gmail.com>
2023-08-06 15:39:28 -0600

    Mostrando información desde LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
-import obtenerProductos from "./API.js";
+// import obtenerProductos from "./API.js";
 
 (function () {
   const resultado = document.querySelector(".producto-grid");
+  const url = "http://localhost:4000/productos";
+
+
+  // Obtiene todo los productos que hay en nuestra API
+  const obtenerProductos = async () => {
+    try {
+      const resultado = await fetch(url)
+      const productos = await resultado.json();
+
+      return productos;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+
 
   let selectedOption;
 
