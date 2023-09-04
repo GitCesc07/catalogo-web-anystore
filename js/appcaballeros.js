ccab02b7e42d90437d998763918588f4604c826f
CescDevGit07 <cesar04071997@gmail.com>
2023-09-03 22:28:50 -0600

    Terminado Catalogo web

--- js/appcaballeros.js
+++ js/appcaballeros.js
@@ -1,5 +1,4 @@
 
-
 (function () {
   const resultado = document.querySelector(".producto-grid");
 
@@ -31,16 +30,13 @@
 
       categroiaload = productos;
 
-      if (categoria === "camisasdamas") {
+      if (categoria === "camisascaballeros") {
 
         const divProductos = document.createElement("div")
 
         divProductos.innerHTML += `
               <div class="producto">
                 <img class="img-producto" src="${img}" alt="${nombre}">
-                <p class="precio">Precio: <span>$${precio}</span></p>
-                <p class="tallas">Tallas: <span>${tallas}</span></p>
-                <p class="tonos">Tonos: <span>${descripcion}</span></p>
                 <div class="border-bottom"></div>
               </div>
             `;
---
+++ js/appniños.js
@@ -0,0 +1,50 @@
+
+
+(function () {
+  const resultado = document.querySelector(".producto-grid");
+
+  let selectedOption, categroiaload;
+
+
+
+  cargarAPi();
+
+  function cargarAPi() {
+    fetch("../db.json")
+      .then(respuesta => respuesta.json())
+      .then(resultado => llenarInformacion(resultado.productos))
+  }
+
+
+  function llenarInformacion(productos) {
+    productos.forEach(producto => {
+      const { categoria, nombre, img, precio, tallas, descripcion, id } = producto;
+
+      console.log(categoria);
+
+      categroiaload = productos;
+
+      if (categoria === "productosniños") {
+
+        const divProductos = document.createElement("div")
+
+        divProductos.innerHTML += `
+              <div class="producto">
+                <img class="img-producto" src="${img}" alt="${nombre}">
+                <div class="border-bottom"></div>
+              </div>
+            `;
+
+        resultado.appendChild(divProductos);
+        return;
+      }
+    });
+  }
+
+  function limpiarHtml() {
+    while (resultado.firstChild) {
+      resultado.removeChild(resultado.firstChild);
+    }
+  }
+
+})();
